06515c2d947511166b227694106d1c8d22a24f16
Carlos <carlos_pedro17@live.com>
2025-03-13 23:33:04 -0300

    commit de ultimo ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-// 1. Importação
-// 2. Codigo do componente
 import { useState } from "react";
 import Perfil from "./components/Perfil";
-import Formulario from "./components/Formulario"
+import style from './App.module.css'
 import ReposList from "./components/ReposList";
 
 function App() {
-  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
-  const [nomeUsuario, setNomeUsuario] = useState('');
+const [nomeUsuario, setNomeUsuario] = useState('');
+const [inputValue, setInputValue] = useState('')
+const handleSearch = () => {setNomeUsuario(inputValue)}
 
-  return (
-    <>
-      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
-
+  return(
+    // uma função commais de um elemento deve ter seu conteúdo dentro de uma tag de encapsulamento ou fragmento
+    // Uma tag de encapsulamento pode ser uma DIV por exemplo, qualquer taga que tnha a função de criar grupos serve.
+    // Já os fragmentos são chaves de Tag vazias "<>", elas podem ser utilizadas assim neste contexto. Apenas para agrupar mas sem agregar nenhum recurso de tag.
+    <div>
+      <div className={style.barraBusca}>
+        <input className={style.input}  value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder='Nome usuario GitHub' />
+        <button className={style.button} onClick={handleSearch}>Buscar</button>
+      </div>
       {nomeUsuario.length > 4 && (
         <>
-          <Perfil nome nomeUsuario={nomeUsuario} />
+          <Perfil nomeUsuario={nomeUsuario} />
           <ReposList nomeUsuario={nomeUsuario} />
-        </ >
+        </>
       )}
-
-      {/* {formularioEstaVisivel && (
-       <Formulario />
-    )}
-   
-    <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">toggle form</button> */}
-    </>
+    </div>
   )
 }
 
